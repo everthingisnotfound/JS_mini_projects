4cf248a98dddf0cd0f6b2fd6403106bd2b618656
Arjun Saxena <saxenaarjun1239@gmail.com>
2024-10-21 22:40:46 +0530

    changes

--- Project1__Background_animation/Background.js
+++ Project1__Background_animation/Background.js
@@ -1,5 +1,4 @@
-for (let i = 1; i<2000; i++){
-    const shape = document.createElement("span")
-    document.querySelector(".container").appendChild(shape)
-
+for (let i = 1; i <= 2000; i++) { // Changed to <= to include 2000
+    const shape = document.createElement("span");
+    document.querySelector(".container").appendChild(shape);
 }
