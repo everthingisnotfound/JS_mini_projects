dfa0d39935be4952e7113ea3f657515b4103b198
Arjun Saxena <saxenaarjun1239@gmail.com>
2024-10-22 11:06:17 +0530

    error corrected

--- Project3__Captcha_Generator/Captcha.js
+++ Project3__Captcha_Generator/Captcha.js
@@ -1,22 +1,42 @@
-document.getElementById("generateButton").addEventListener("click", () => {
-    const input = document.getElementById("userInput").value.trim();
-    const lettersContainer = document.getElementById("lettersContainer");
+const display = document.getElementById("_status");
+const body = document.body;
+const submit = document.getElementById("_submit");
+const refresh = document.getElementById("_refresh");
+const char = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuuvwxyz";
+let captcha = "";
 
-    // Clear previous letters
-    lettersContainer.innerHTML = '';
 
-    // Create spans for each character in the input, including spaces
-    for (const letter of input) {
-        const span = document.createElement("span");
-        span.textContent = letter === ' ' ? '\u00A0' : letter; // Use non-breaking space for better spacing
-        lettersContainer.appendChild(span);
+body.onload = function generateCaptcha(){
+    const captchaLength = 6;
+    for(let i = 0; i < captchaLength; i++){
+        const randomIndex = Math.floor(Math.random() * char.length)
+        captca += char.substring(randomIndex, randomIndex + 1)
+    }
+    document.getElementById("_generator").value = captcha;
+    display.innerText = "Captcha Generator";
+}
+
+submit.onclick = function checkInput(){
+    const input = document.getElementById("_client-text").value;
+    if(input === ""){
+        display.innerText = "👇Pleaase Enter the text shown Below👇";
+    }
+    else if(input === captcha){
+        display.innerText = "👍 Correct! You have passed the Captcha Test!"
+    }
+    else{
+        display.innerText = "🚫 Incorrect! Try again!"
+    }
+}
+
+refresh.onclick = function refreshCaptcha(){
+    let newCaptcha = "";
+    for(let i = 0; i < captchaLength; i++){
+    const randomChar = char[Math.floor(MMath.random() * char.length)]
+    newCaptcha += randomChar;
     }
 
-    // Trigger automatic jumping animation
-    const spans = lettersContainer.querySelectorAll("span");
-    spans.forEach((span, index) => {
-        setTimeout(() => {
-            span.classList.add("active");
-        }, index * 500); // Delay each letter's animation by 500ms
-    });
-});
+    captcha = newCaptcha;
+    document.getElementById("_generator").value = captcha;
+    display.innerText = "Captcha Generator";
+}
